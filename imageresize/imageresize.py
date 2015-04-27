444901462260633c035714ec40499afb89486d0f
charlesthk <ct@e-reflex.fr>
2015-04-27 18:00:34 +0200

    adding tests

--- imageresize/imageresize.py
+++ imageresize/imageresize.py
@@ -1,13 +1,13 @@
+from __future__ import division
 from PIL import Image
-import math
 
 def crop(image, size):
 	img_format = image.format
 	old_size = image.size
-	left = (size[0] - old_size[0])/2
-	top = (size[1] - old_size[1])/2
-	right = size[0] - left
-	bottom = size[1] - top
+	left = int((old_size[0] - size[0])/2)
+	top = int((old_size[1] - size[1])/2)
+	right = int(old_size[0] - left)
+	bottom = int(old_size[1] - top)
 	crop = image.crop((left, top, right, bottom))
 	crop.format = img_format
 	return crop
@@ -17,14 +17,14 @@ def resize_cover(image, size):
 	Resize image according to size.
 	size is a list of two values, respectively [width, height]
 	"""
+	img_format = image.format
 	img = image.copy()
 	img_size = img.size
 	if (img_size[0] >= size[0] and img_size[1] >= size[1]):
-		img_format = img.format
-		ratio = max(size[0]/float(img_size[0]), size[1]/float(img_size[1]))
-		new_size = [int(math.ceil(img_size[0]*ratio)), int(math.ceil(img_size[1]*ratio))]
+		ratio = max(size[0]/img_size[0], size[1]/img_size[1])
+		new_size = [int(img_size[0]*ratio), int(img_size[1]*ratio)]
 		img = img.resize((new_size[0],new_size[1]), Image.LANCZOS)
-		img = crop(img, size, img.size)
+		img = crop(img, size)
 		img.format = img_format
 		return img
 	else:
@@ -35,15 +35,16 @@ def resize_contain(image, size):
 	Resize image according to size.
 	size is a list of two values, respectively [width, height]
 	"""
+	img_format = image.format
 	img = image.copy()
 	img_size = img.size
 	if (img_size[0] >= size[0] or img_size[1] >= size[1]):
-		image_format = img.format
 		img.thumbnail((size[0], size[1]), Image.LANCZOS)
 		background = Image.new('RGBA', (size[0], size[1]), (255, 255, 255, 0))
-		img = background.paste(img, ((size[0] - img.size[0]) / 2, (size[1] - img.size[1]) / 2))
-		img.format = img_format
-		return img
+		backgroung_size = (int((size[0] - img.size[0]) / 2), int((size[1] - img.size[1]) / 2))
+		background.paste(img, backgroung_size)
+		background.format = img_format
+		return background
 	else:
 		raise ValueError(u"Your image is too small. Minimum size: %s" % size)
 
@@ -52,11 +53,11 @@ def resize_by_width(image, width):
 	Resize image according to size.
 	size is a list of two values, respectively [width, height]
 	"""
+	img_format = image.format
 	img = image.copy()
 	img_size = img.size
 	if (img_size[0] >= width):
-		new_height = math.ceil((width/img_size[0])*img_size[1])
-		image_format = img.format
+		new_height = int((width/img_size[0])*img_size[1])
 		img.thumbnail((width, new_height), Image.LANCZOS)
 		img.format = img_format
 		return img
@@ -68,12 +69,12 @@ def resize_by_height(image, height):
 	Resize image according to size.
 	size is a list of two values, respectively [width, height]
 	"""
+	img_format = image.format
 	img = image.copy()
 	img_size = img.size
 	if (img_size[1] >= height):
-		new_width = math.ceil((height/img_size[1])*img_size[0])
-		image_format = img.format
-		img.thumbnail((height, new_width), Image.LANCZOS)
+		new_width = int((height/img_size[1])*img_size[0])
+		img.thumbnail((new_width, height), Image.LANCZOS)
 		img.format = img_format
 		return img
 	else:
@@ -84,7 +85,7 @@ def resize_thumbnail(image, size):
 	Resize image according to size.
 	size is a list of two values, respectively [width, height]
 	"""
-	image_format = img.format
+	img_format = image.format
 	img = image.copy()
 	img.thumbnail((size[0], size[1]), Image.LANCZOS)	
 	img.format = img_format
---
+++ tests/tests.py
@@ -0,0 +1,34 @@
+from imageresize import imageresize
+from cStringIO import StringIO
+from PIL import Image
+
+test_img = open('test-image.jpeg', 'rw')
+
+img = Image.open(test_img)
+img = imageresize.crop(img, [200, 200])
+img.save('test-image-crop.jpeg', img.format)
+
+
+img = Image.open(test_img)
+img = imageresize.resize_cover(img, [200, 100])
+img.save('test-image-cover.jpeg', img.format)
+
+
+img = Image.open(test_img)
+img = imageresize.resize_contain(img, [200, 100])
+img.save('test-image-contain.jpeg', img.format)
+
+
+img = Image.open(test_img)
+img = imageresize.resize_by_width(img, 200)
+img.save('test-image-width.jpeg', img.format)
+
+
+img = Image.open(test_img)
+img = imageresize.resize_by_height(img, 200)
+img.save('test-image-height.jpeg', img.format)
+
+
+img = Image.open(test_img)
+img = imageresize.resize_thumbnail(img, [200, 200])
+img.save('test-image-thumbnail.jpeg', img.format)
